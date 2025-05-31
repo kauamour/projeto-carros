74827c43efc7536bca9e39bfd8d3beb0e4d9cc60
kauamour <kaumouraff@gmail.com>
2025-05-31 16:02:51 -0300

    projeto finalizado

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let container = document.querySelector('.container')
 let itens = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
 
 let active = 0
 let firstPosition = 0
@@ -22,6 +23,7 @@ function setSlider() {
 }
 
 prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
     active = active - 1 < firstPosition ? lastPosition : active - 1
     setSlider()
     itens [active].classList.add('active')
@@ -30,6 +32,7 @@ prevButton.onclick = () => {
 }
 
 nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
     active = active + 1 > lastPosition ? 0 : active + 1
     setSlider()
     itens [active].classList.add('active')
